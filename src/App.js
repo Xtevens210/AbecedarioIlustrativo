bc444de905a951233174c214497c8aa01496907b
xtevens210 <stevens210@hotmail.es>
2022-04-01 00:59:08 -0500

    Inicio del proyecto primeros pasos, componente ficha creado y funcionando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Ficha from './components/Ficha';
+import LetraA from './Imagenes/carta-a.png';
+import Avion from './Imagenes/avion.png'
+import LetraB from './Imagenes/letra-b.png'
+import Ballena from './Imagenes/ballena.png'
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Ficha icono={LetraA} icono2={Avion} nombreIcono="Avion" name="Letra A"/>
+      <Ficha icono={LetraB} icono2={Ballena} nombreIcono="Ballena" name="Letra B"/>
     </div>
   );
 }
